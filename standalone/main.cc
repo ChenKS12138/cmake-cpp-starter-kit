769bec7c948ac65fa2b212434d6265f3dd3a313f
ChenKS12138 <chenks12138@gmail.com>
2023-06-05 22:52:11 +0800

    feat: update example

--- standalone/main.cc
+++ standalone/main.cc
@@ -2,6 +2,7 @@
 #include <string>
 #include <vector>
 
+#include "absl/base/log_severity.h"
 #include "absl/log/globals.h"
 #include "absl/log/initialize.h"
 #include "absl/log/log.h"
@@ -11,13 +12,16 @@
 int main() {
     using namespace std;
 
+    absl::SetStderrThreshold(absl::LogSeverity::kInfo);
     absl::InitializeLog();
 
     int ret = Foo::add(1, 2);
-    cout << absl::StrFormat("1+1=%d", ret) << endl;
+    LOG(INFO) << absl::StrFormat("1+1=%d", ret) << endl;
 
     vector<string> v{"aaa", "bbb", "cccc"};
-    LOG(ERROR) << Foo::JoinList(v);
+    LOG(INFO) << Foo::JoinList(v);
+
+    LOG(INFO) << "done"s;
 
     return 0;
 }
